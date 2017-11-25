15137077415159c0a74e7d22293bcbfd6768af23
Radoslav <radoslavhlinka@gmail.com>
2017-11-25 19:47:28 +0100

    Alamofire

--- Weather/Controllers/CityViewController.swift
+++ Weather/Controllers/CityViewController.swift
@@ -10,26 +10,22 @@ import UIKit
 
 class CityViewController: UIViewController {
 
+    @IBOutlet weak var cityLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func getCityTapped(_ sender: Any) {
+        //1 Get the city name the user entered in the text field
+        
+        //2 If we have a delegate set, call the method userEnteredANewCityName
+        
+        //3 dismiss the Change City View Controller to go back to the WeatherViewController
+        
     }
-    */
-
+    
+    @IBAction func backTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
--- Weather/Controllers/WeatherController.swift
+++ Weather/Controllers/WeatherController.swift
@@ -7,11 +7,54 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-class WeatherController: UIViewController {
-
+class WeatherController: UIViewController, CLLocationManagerDelegate {
+    
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func getWeatherData(url: String, parameters: [String: String]) {
+        Alamofire.request(url, parameters: parameters).responseJSON { response in
+            print(response)
+            response.
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count-1]
+        if location.horizontalAccuracy > 0 {
+            manager.stopUpdatingLocation()
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String:String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location unavailable. 😢"
     }
 }
 
